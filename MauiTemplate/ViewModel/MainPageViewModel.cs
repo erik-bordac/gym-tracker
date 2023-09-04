4c8ac28ca5cdaa4285fdf3fd66f74a6a5267fe82
bordace <erik.bordac@gmail.com>
2023-09-04 19:12:12 +0200

    fix issue #10

--- MauiTemplate/ViewModel/MainPageViewModel.cs
+++ MauiTemplate/ViewModel/MainPageViewModel.cs
@@ -1,22 +1,75 @@
 ﻿
 using GymTracker.Services;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
 
 namespace GymTracker.ViewModel;
 public partial class MainPageViewModel : BaseViewModel
 {
 	[ObservableProperty]
+	private IEnumerable<ISeries> ratioSeries;
+
+	[ObservableProperty]
 	private bool ongoingRoutine;
 
 	private OngoingRoutineService _ors;
+	private LocalDatabase _db;
 
-	public MainPageViewModel(OngoingRoutineService ors) 
+	public MainPageViewModel(OngoingRoutineService ors, LocalDatabase db) 
 	{ 
 		_ors = ors;
+		_db = db;
 	}
 
 	public void OnAppearing()
 	{
 		OngoingRoutine = _ors.OngoingRoutine;
+
+		// Load Work/Rest series
+		Task.Run(async () =>
+		{
+			var historyList = await _db.GetExerciseHistoryListAsync();
+			var _30daysAgo = DateTime.Now.Date.AddDays(-30);
+
+			bool[] workDays = new bool[31];
+
+			foreach(var item in historyList)
+			{
+				var diff = item.DateTime.Date - DateTime.Now.Date.AddDays(-30);
+				int dayDiff = diff.Days;
+
+				if (dayDiff >= 0)
+				{
+					workDays[dayDiff] = true;
+				}
+			}
+
+			int workDaysCount = 0;
+			foreach(var value in workDays)
+			{
+				if (value)
+				{
+					workDaysCount++;
+				}
+			}
+
+			RatioSeries = new[] { 30-workDaysCount, workDaysCount }.AsPieSeries((value, series) =>
+			{
+				// pushes out the slice with the value of 6 to 30 pixels.
+				if(value==workDaysCount)
+				{
+					series.Pushout = 10;
+					series.Name = "Work";
+					series.Fill = new SolidColorPaint(new SKColor(212, 173, 252));
+				} else
+				{
+					series.Name = "Rest";
+					series.Fill = new SolidColorPaint(new SKColor(43, 43, 43));
+				}
+			});
+		});
 	}
 
 	[RelayCommand]
