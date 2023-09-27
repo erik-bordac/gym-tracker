ee0453e18a779872622d438ab76b8e10f2c1f101
bordace <erik.bordac@gmail.com>
2023-09-27 15:50:46 +0200

    import functionality

--- MauiTemplate/ViewModel/ImportExportPageViewModel.cs
+++ MauiTemplate/ViewModel/ImportExportPageViewModel.cs
@@ -1,14 +1,54 @@
-﻿using GymTracker.Services;
+﻿using GymTracker.Helpers;
+using GymTracker.Services;
 using GymTracker.View;
+using System.IO;
 
 namespace GymTracker.ViewModel;
 
 public partial class ImportExportPageViewModel : BaseViewModel
 {
-	public int Tmp { get; set; }
-
 	public ImportExportPageViewModel()
 	{
-		Tmp = 50;
+
+	}
+
+	[RelayCommand]
+	public async Task LoadFile()
+	{
+		var customFileType = new FilePickerFileType(
+				new Dictionary<DevicePlatform, IEnumerable<string>>
+				{
+                    { DevicePlatform.Android, new[] { ".db3" } },
+                    { DevicePlatform.WinUI, new[] { ".db3" } },
+                });
+
+		PickOptions options = new()
+		{
+			PickerTitle = "Please select a .db3 file",
+			FileTypes = customFileType,
+		};
+
+		var result = await PickAndShow(options);
+		
+		if (result != null)
+		{
+			System.IO.File.Copy(result.FullPath, Constants.DatabasePath, true);
+		}
+	}
+
+	private async Task<FileResult> PickAndShow(PickOptions options)
+	{
+		try
+		{
+			var result = await FilePicker.Default.PickAsync(options);
+
+			return result;
+		}
+		catch (Exception ex)
+		{
+			await Application.Current.MainPage.DisplayAlert("Exception", $"{ex.Message}", "Ok");
+		}
+
+		return null;
 	}
 }
