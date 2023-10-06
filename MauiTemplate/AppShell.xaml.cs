fda58c4cb5b2576ea4bbbd7a4796c7c7e45f15f4
bordace <erik.bordac@gmail.com>
2023-10-06 13:43:52 +0200

    add themes

--- MauiTemplate/AppShell.xaml.cs
+++ MauiTemplate/AppShell.xaml.cs
@@ -19,4 +19,15 @@ public partial class AppShell : Shell
 		Routing.RegisterRoute("ExerciseHistoryPage", typeof(ExerciseHistoryPage));
 		Routing.RegisterRoute("ImportExportPage", typeof(ImportExportPage));
 	}
+
+	private void SwitchToggled(object sender, ToggledEventArgs e)
+	{
+		if (e.Value)
+		{
+			Application.Current.UserAppTheme = AppTheme.Dark;
+		} else
+		{
+			Application.Current.UserAppTheme = AppTheme.Light;
+		}
+	}
 }
